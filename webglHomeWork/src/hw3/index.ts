2605aebab1947a7ab463ec16f31f48305fb00577
liuyi0703 <1808156048@qq.com>
2020-12-09 21:17:24 +0800

    material

---
+++ webglHomeWork/src/hw3/index.ts
@@ -0,0 +1,103 @@
+import { Canvas } from "../../utils/canvas"
+import { Camera, ControlledCamera } from "../../utils/core/camera"
+import { DepthTextureTarget, CanvasTarget } from "../../utils/core/renderTarget"
+import { RenderContext, Renderer } from "../../utils/core/render"
+import { Container } from "../../utils/core/displayObject"
+import { ShadowTexurePlugin } from "../../utils/plugins/ShadowTexture"
+import { resizeCanvas } from "../../utils/helpers/WebGlUtils"
+import { Vector3 } from "../../utils/math/Vector3"
+import { SphereObject } from "../../utils/shapes/Sphere"
+import { TextureSphereObject } from "../../utils/plugins/TextureSphere";
+import { TextureBase } from "../../utils/core/texture";
+import { LoadImageAsync } from "../../utils/helpers/asyncLoad";
+import { TextureCubeObject } from "../../utils/shapes/TextureCube"
+import { LightBall } from "./lightBall"
+import earthSrc from "../assets/earth.jpg"
+import "../../utils/draw/3D_model/index"
+
+
+async function main() {
+
+    let CanvaS = new Canvas("canvas")
+
+    const { gl } = CanvaS
+
+    const canvas = CanvaS.canvas
+
+    gl.enable(gl.DEPTH_TEST)
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    const stage = new Container()
+    const camera = new ControlledCamera(canvas, false)
+
+    const lightBall = new LightBall(gl)
+
+    camera
+        .setViewPort(canvas.width / canvas.height)
+        .setPosition(0, 0, 20)
+        .setLookAt(0, 0, 0)
+    const light = new Camera()
+        .setViewPort(1, 0.5 * Math.PI)
+        .setPosition(12, 0, 0)
+        .setLookAt(0, 0, 0)
+
+    const earthTexture = new TextureBase(gl, await LoadImageAsync(earthSrc))
+
+    let ball = new TextureSphereObject(earthTexture)
+
+    ball.position.set(0, 0, 0)
+    ball.scale.set(7, 7, 7)
+    ball.color.set(0x4cd1e0)
+
+    let sun = new SphereObject()
+
+    sun.position.set(10, 10, 0)
+    sun.scale.set(0.2, 0.2, 0.2)
+    sun.color.set(0xe2c074)
+
+    let prev = performance.now() / 1000
+
+    function update(now: number) {
+        now = now / 1000
+        const dt = now - prev
+        if (resizeCanvas(gl, canvas))
+            camera.setViewPort(canvas.width / canvas.height)
+
+        let speed = <HTMLInputElement>document.getElementById("speed")
+        lightBall.animate(dt, speed.value)
+        light.position.set(20 * lightBall.sun.position.x, 20 * lightBall.sun.position.y, lightBall.position.z)
+        // light.setLookAt(20 * lightBall.sun.position.x, 20 * lightBall.sun.position.y, lightBall.position.z)
+        document.body.style.backgroundImage = "linear-gradient(-" + lightBall.theta / (2 * Math.PI) * 360 + "deg, #cdd1d3,#1677b3,#131824 55%,#0B1013)"
+        console.log("linear-gradient(" + (0 - lightBall.theta / (2 * Math.PI) * 360) + "deg, #22a6f1,black 50%)")
+        camera.update(dt)
+        prev = now
+    }
+
+    const renderer = new Renderer(gl)
+    const canvastarget = new CanvasTarget(gl)
+    const depthtarget = new DepthTextureTarget(gl, 4096, 4096)
+
+    const context: RenderContext = {
+        camera,
+        light,
+        shadowTexture: depthtarget.texture
+    }
+
+    stage.addChild(ball)
+    // stage.addChild(sun)
+    stage.addChild(lightBall)
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+    function tick(now: number) {
+        requestAnimationFrame(tick)
+        update(now)
+        light.updateTransform()
+        // renderer.render(depthtarget, stage, context, true, ShadowTexurePlugin.PluginName)
+        renderer.render(canvastarget, stage, context, true)
+    }
+    requestAnimationFrame(tick)
+}
+
+main()
+
---
+++ webglHomeWork/utils/shapes/Sphere.ts
@@ -0,0 +1,109 @@
+import { Container } from "../core/displayObject"
+import { Vector3 } from "../math/Vector3"
+import { BindableBuffer } from "../core/bindableBuffer"
+import { ICommonShadowObject, CommonShadowPlugin } from "../plugins/CommonShadow"
+import { IShadowTexureObject } from "../plugins/ShadowTexture"
+import { ISolidFaceObject, SolidFacePlugin } from "../plugins/SolidFace"
+
+function GetPoint(jingdu: number, weidu: number) {
+
+  const cosweidu = Math.cos(weidu)
+  const dx = Math.cos(jingdu) * cosweidu
+  const dy = Math.sin(jingdu) * cosweidu
+  const dz = Math.sin(weidu)
+
+  return new Vector3(dx, dy, dz)
+}
+
+function calculateCircleVertices(division: number) {
+
+  const vertices: Vector3[] = []
+
+  const div = division > 0 ? division : 1
+
+  const circles: [Vector3[], Vector3[]] = [[], []]
+
+  const jingdus: number[] = []
+  const weidus: number[] = []
+
+  for (let i = 0; i < div * 4; i++) {
+    const jingdu = i / div / 2 * Math.PI
+    jingdus.push(jingdu)
+  }
+  for (let i = 1; i <= div; i++) {
+    const weidu = i / div / 2 * Math.PI
+    weidus.push(weidu)
+  }
+
+  jingdus.forEach(j => {
+    circles[0].push(GetPoint(j, 0))
+  })
+
+  weidus.forEach((w, i) => {
+    const outer = circles[i % 2]
+    const inner = circles[1 - i % 2]
+
+    inner.length = 0
+    jingdus.forEach(j => {
+      inner.push(GetPoint(j, w))
+    })
+
+    for (let i = 0; i < inner.length; i++) {
+      const ii = (i + 1) % inner.length
+
+      let verts = [outer[i], outer[ii], inner[ii], inner[i],]
+      verts = verts.concat(verts.map(x => new Vector3(x.x, x.y, -x.z)))
+
+      const order = [0, 1, 2, 0, 2, 3, 4, 6, 5, 4, 7, 6]
+      order.forEach(x => vertices.push(verts[x]))
+    }
+  })
+
+  return vertices
+}
+
+function getCirCleVertexDataBuffer() {
+  const vertices = calculateCircleVertices(20)
+
+  const positionBuffer = new Float32Array(vertices.length * 3)
+
+  let i = 0
+  let j = 0
+  while (i < vertices.length) {
+    const v0 = vertices[i++]
+    v0.toArray(positionBuffer, j)
+    j += 3
+  }
+
+  return positionBuffer
+}
+
+const normalCircleVertexBuffer = new BindableBuffer(getCirCleVertexDataBuffer().buffer)
+
+export class SphereObject extends Container implements ICommonShadowObject, IShadowTexureObject {
+
+  plugin = CommonShadowPlugin.PluginName
+  shadowRender = true
+  material= {
+    specular: 0.8,
+    diffuse: 4,
+    ambient: 0.8
+  }
+  data = {
+    positionBuffer: normalCircleVertexBuffer,
+    normalBuffer: normalCircleVertexBuffer,
+    points: normalCircleVertexBuffer.arrayBuffer.byteLength / 3 / 4
+  }
+}
+
+export class SolidShpereObject extends Container implements ISolidFaceObject {
+
+  plugin = SolidFacePlugin.PluginName
+  
+  data = {
+    positionBuffer: normalCircleVertexBuffer,
+    normalBuffer: normalCircleVertexBuffer,
+    points: normalCircleVertexBuffer.arrayBuffer.byteLength / 3 / 4
+  }
+}
+
